170ccb73641111fe374ed97df823876847104628
Fhiminsa <mukminhamzah30@gmail.com>
2024-01-02 22:36:17 +0800

    Add a query to determine the outcome of Barcelona's matches where they played as the away team

--- SQL for European Soccer Database/CASE Statement Comparing Column Values.sql
+++ SQL for European Soccer Database/CASE Statement Comparing Column Values.sql
@@ -16,3 +16,20 @@ LEFT JOIN Team AS t
 on m.away_team_api_id = t.team_api_id
 -- Filter for Barcelona as the home team
 WHERE m.home_team_api_id = 8634;
+
+
+'''
+	Similar to the previous exercise, We will construct a query to determine the outcome of Barcelona"s 
+    matches where they played as the away team.
+'''
+SELECT 
+	m.date,
+	t.team_long_name AS opponent,
+	CASE WHEN m.home_team_goal < m.away_team_goal THEN 'Barcelona Win'
+		WHEN m.home_team_goal > m.away_team_goal THEN 'Barcelona Lose'
+		ELSE 'Tie' END AS Outcome
+FROM Match AS m
+LEFT JOIN Team AS t
+on m.away_team_api_id = t.team_api_id
+-- Filter for Barcelona as the home team
+WHERE m.away_team_api_id = 8634;
