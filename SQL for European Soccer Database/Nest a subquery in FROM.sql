3cbd0f54c8e9eb08a1b23a438d221ce7e30f509d
Fhiminsa <mukminhamzah30@gmail.com>
2024-02-01 21:03:32 +0800

    Update Nested simple subqueries

--- SQL for European Soccer Database/Nest a subquery in FROM.sql
+++ SQL for European Soccer Database/Nest a subquery in FROM.sql
@@ -1,8 +1,9 @@
 SELECT 
 	c.name AS country,
-	
+	-- Calculate the average matches per season
 	AVG(outer_subq.matches) AS avg_seasonal_high_scores	
 FROM Country c 
+-- Left join outer_s to country
 LEFT JOIN (
 	SELECT 
 		country_id,
@@ -15,6 +16,7 @@ LEFT JOIN (
 			id
 		FROM MATCH
 		WHERE home_team_goal>=5 OR away_team_goal<=5) AS inner_subq
+	-- Close parentheses and alias the subquery
 	GROUP BY country_id,season) AS outer_subq
 ON c.id = outer_subq.country_id
 GROUP BY country;
