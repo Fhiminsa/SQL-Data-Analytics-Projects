005432e6fb18aca13c473bc2aa257b5b29012eb8
Fhiminsa <mukminhamzah30@gmail.com>
2024-02-08 08:44:37 +0800

    Add PARTITION BY clause

---
+++ SQL for European Soccer Database/PARTITION BY a column.sql
@@ -0,0 +1,16 @@
+SELECT 
+	id,
+	date,
+	season,
+	home_team_goal,
+	away_team_goal,
+	
+	CASE WHEN home_team_api_id = 8673 THEN 'Home'
+		ELSE 'Away' END AS warsaw_location,
+	
+	AVG(home_team_goal) OVER(PARTITION BY season)AS season_homeavg,
+	AVG(away_team_goal) OVER(PARTITION BY season) AS season_awayavg
+
+FROM Match
+WHERE home_team_api_id = 8673 OR away_team_api_id = 8673
+ORDER BY (home_team_goal + away_team_goal) DESC;
